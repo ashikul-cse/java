b5c50f46712a48d27516df8f1c4135a6fde249b9
ashikul-cse <60182008+ashikul-cse@users.noreply.github.com>
2020-07-14 01:08:35 +0600

    URI 1001

---
+++ UriOne.java
@@ -0,0 +1,15 @@
+package uriproblems;
+
+import java.util.Scanner;
+
+public class UriOne {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int A, B, X;
+        A = input.nextInt();
+        B = input.nextInt();
+        X=A+B;
+        System.out.println("X = "+X+"\n");
+    }
+}
